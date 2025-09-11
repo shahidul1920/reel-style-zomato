b5da41fcbc4d2c9fec89c692609bef71e33e1043
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-09-12 01:37:15 +0600

    damn you error

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectDB() {
-   mongoose.connect(process.env.MONGO_BD)
+   mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
       console.log("Database connected successfully");
     })
