f72016e5c70897b03ffc4852c6d0cd29b3add1fa
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-10-05 22:59:49 +0600

    imagekit tested

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,9 +1,13 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.services')
+const {v4:uuid} = require('uuid')
 
 async function createFood(req, res) {
     //console.log(req.foodPartner);
-    console.log(req.body);
-    console.log(req.file);
+    //console.log(req.body);
+    //console.log(req.file);
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid())
+    console.log(fileUploadResult);
     res.send("open ur fuckin eyes")
 }
 
--- backend/src/services/storage.services.js
+++ backend/src/services/storage.services.js
@@ -5,3 +5,15 @@ const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
+
+async function uploadFile (file, fileName){
+    const result = await imagekit.upload({
+        file: file,
+        fileName: fileName
+    })
+    return result;
+}
+
+module.exports = {
+    uploadFile
+}
