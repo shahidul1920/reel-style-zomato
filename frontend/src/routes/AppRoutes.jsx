fa77ec0813c87eceb78661939b7c2c9a2ed7497c
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-10-19 12:38:17 +0600

    Integrate Tailwind CSS with Vite and update routes
    Added Tailwind CSS and its Vite plugin to the project for styling support. Updated index.css to import Tailwind. Modified AppRoutes to include new user and food-partner login/register routes, with Tailwind styling applied to one route.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,7 +5,10 @@ export const AppRoutes = () => {
   return (
     <Router>
         <Routes>
-            <Route path='/user/res' element={<h1>hello reg</h1>} />
+            <Route path='/user/register' element={<h1>hello register</h1>} />
+            <Route path='/user/login' element={<h1>hello login</h1>} />
+            <Route path='/food-partner/register' element={<h1 className='text-5xl'>food-partner register</h1>} />
+            <Route path='/food-partner/login' element={<h1>food-partner login</h1>} />
         </Routes>
     </Router>
   )
